71bd539fb9983dd4989978dda84ff256e83848be
Andrew Piroli <45859969+AndrewPiroli@users.noreply.github.com>
2021-02-08 10:53:21 -0500

    Maybe I'm starting to understand

--- DiscordOperations.py
+++ DiscordOperations.py
@@ -2,8 +2,11 @@ import discord
 from enum import Enum, auto
 from pprint import pprint
 from pathlib import Path
+import uuid
+import asyncio
+from typing import List
 max_attach_size = 10240 # 10 KB
-storage = Path("./temp")
+storage = Path("temp").absolute()
 
 class MessageStatus(Enum):
     OK = auto()
@@ -14,6 +17,23 @@ class MessageStatus(Enum):
 
 
 client = discord.Client()
+async_tasks = []
+active_files = []
+
+async def create_file(attachment: discord.Attachment):
+    filename = f"{uuid.uuid4()}.txt" # This *probably* won't collide ¯\_(ツ)_/¯ I'll write a check later
+    with open(storage / filename, "wb") as syscheck_file:
+        await attachment.save(syscheck_file)
+        active_files.append(filename)
+    
+async def clean_tasks(tasklist: List[asyncio.Task]):
+    to_pop = []
+    for idx,task in enumerate(tasklist):
+        if task.done():
+            await task
+            to_pop.append(idx)
+    for idx in to_pop:
+        tasklist.pop(idx)
 
 @client.event
 async def on_ready():
@@ -21,11 +41,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    janitor = asyncio.create_task(clean_tasks(async_tasks))
     if message.author == client.user:
         return
     if message.attachments:
         print("yeet")
-        attachment = message.attachments[0]
+        attachment: discord.Attachment = message.attachments[0]
     else:
         return
     if attachment.width:
@@ -34,7 +55,9 @@ async def on_message(message):
     if attachment.size > max_attach_size:
         print('too big')
         return
-    
+    if "syscheck" in attachment.filename:
+        async_tasks.append(asyncio.create_task(create_file(attachment)))
+    await janitor
 
 
 client.run(open("private-discord-token.txt", "r").read().strip())
